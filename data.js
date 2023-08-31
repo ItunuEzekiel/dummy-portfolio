b542dd16e51aafead9dc2880e675eeddabd48aa6
ItunuEzekiel <56870995+ItunuEzekiel@users.noreply.github.com>
2023-08-31 23:17:39 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,17 @@
+const servicesData = [
+    {
+        title: "Frontend Development",
+        category: "frontend",
+        description: ['Frontend Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. ']
+    },
+    {        
+        title: "Backend Development",
+        category: "backend",
+        description: ['BackendId nibh tortor id aliquet lectus proin nibh nisl. Suspendisse ultrices gravida dictum fusce ut placerat orci nulla. Dui vivamus arcu felis bibendum ut. Sed tempus urna et pharetra pharetra massa massa ultricies mi. Bibendum ut tristique et egestas quis ipsum suspendisse. Nunc congue nisi vitae suscipit tellus mauris a diam maecenas. Non quam lacus suspendisse faucibus. Pharetra vel turpis nunc eget lorem dolor sed viverra ipsum. Egestas tellus rutrum tellus pellentesque eu tincidunt. Nec dui nunc mattis enim ut tellus elementum sagittis vitae. Praesent semper feugiat nibh sed pulvinar proin gravida hendrerit.']
+    },
+    {
+        title: "Fullstack Development",
+        category: "fullstack",
+        description: ['Fullstack Ullamcorper malesuada proin libero nunc consequat. Ut pharetra sit amet aliquam id diam maecenas ultricies. Lectus quam id leo in vitae. Vulputate ut pharetra sit amet aliquam id diam. Facilisis leo vel fringilla est ullamcorper eget. Vitae congue mauris rhoncus aenean vel elit scelerisque mauris pellentesque. Tellus molestie nunc non blandit massa enim. Aliquam ultrices sagittis orci a scelerisque. Tincidunt vitae semper quis lectus nulla at. Vitae tempus quam pellentesque nec nam aliquam sem. Morbi tempus iaculis urna id volutpat lacus. Nulla aliquet porttitor lacus luctus accumsan tortor posuere. Sem integer vitae justo eget. Sit amet risus nullam eget felis eget nunc. Egestas quis ipsum suspendisse ultrices gravida dictum fusce. Ornare massa eget egestas purus viverra accumsan in nisl nisi. Tincidunt ornare massa eget egestas purus viverra. Penatibus et magnis dis parturient montes nascetur ridiculus mus mauris. Nullam vehicula ipsum a arcu cursus vitae congue mauris.']
+    }
+]
---
+++ index.js
@@ -0,0 +1,127 @@
+//======================= SERVICES TOGGLE
+
+const servicesButton = document.querySelectorAll('.service__item');
+const servicesDetails = document.querySelector('.services__right');
+
+const getService = (category) => {
+    const details = servicesData.find(item => item.category === category);
+    servicesDetails.innerHTML = `
+    <h3>${details.title}</h3>
+    ${details.description.map(paragraph => "<p>" + paragraph + "</p>").join('')}
+`
+}
+
+const removeActiveClass = () => {
+    servicesButton.forEach(button => {
+        button.classList.remove('active');
+    })
+}
+
+
+servicesButton.forEach(item => {
+    item.addEventListener('click', () => {
+        const serviceClass = item.classList[1];
+        getService(serviceClass);
+        item.classList.add('active');
+    })
+});
+
+getService('frontend');
+
+
+// =================================MIXITUP (projects section)
+const containerE1 = document.querySelector('.projects__container');
+var mixer = mixitup(containerE1, {
+    animation: {
+        enable: false
+    }
+});
+
+
+mixer.filter('*');
+
+
+
+
+// ====================SWIPER (testimonials section)
+
+const swiper = new Swiper('.swiper', {
+
+   slidesPerView: 1,
+   spaceBetween: 30,
+   pagination: {
+        el: ".swiper-pagination",
+        clickable: true
+   },
+
+    breakpoints: {
+        600: {
+            slidesPerView: 2
+        },
+        1024: {
+            slidesPerView: 3
+        }
+    }
+  });
+
+
+//===================================NAV TOGGLE (small screen)
+
+const navMenu = document.querySelector('.nav__menu');
+const navOpenBtn = document.querySelector('.nav__toggle-open');
+const navCloseBtn = document.querySelector('.nav__toggle-close');
+
+
+const openNavHandler = () => {
+    navMenu.style.display = 'flex';
+    navOpenBtn.style.display = 'none';
+    navCloseBtn.style.display = 'inline-block';
+}
+
+
+const closeNavHandler = () => {
+    navMenu.style.display = 'none';
+    navOpenBtn.style.display = 'inline-block';
+    navCloseBtn.style.display = 'none';
+}
+
+
+navOpenBtn.addEventListener('click', openNavHandler);
+navCloseBtn.addEventListener('click', closeNavHandler);
+
+
+// close nav menu on click of nav link on small screens
+const navItems = navMenu.querySelectorAll('a');
+if(window.innerWidth < 768) {
+    navItems.forEach(item => {
+        item.addEventListener('click', closeNavHandler)
+    })
+}
+
+//=============================== THEME TOGGLE (light & dark mode)
+const themeBtn = document.querySelector('.nav__theme-btn');
+themeBtn.addEventListener ('click', () => {
+    let bodyClass = document.body.className;
+    if(!bodyClass) {
+        bodyClass = 'dark';
+        document.body.className = bodyClass;
+        // change toggle icon
+        themeBtn.innerHTML = "<i class='uil uil-sun'></i>"
+        // save theme to local storage
+        window.localStorage.setItem('theme', bodyClass);
+    }else {
+        bodyClass = '';
+        document.body.className = bodyClass;
+        // change toggle icon
+        themeBtn.innerHTML = "<i class='uil uil-moon'></i>"
+        // save theme to local storage
+        window.localStorage.setItem('theme', bodyClass);
+
+    }
+})
+
+
+// load theme on load
+window.addEventListener('load', () => {
+    document.body.className = window.localStorage.getItem('theme');
+})
